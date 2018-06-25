fc3068f67ba516175d2775cefc1602fc0680b419
Kongjianqiu <kongjianqiu@gmail.com>
2018-06-24 23:00:33 -0500

    Update tosession.py

--- dataprocess/tosession.py
+++ dataprocess/tosession.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 
 df = pd.read_csv('../data/chat.csv')[['session_id','user_id','send','zhaunchu','repeat','sku','content']]
+UNK='UNK'
 temp = 0
 allpairs = {}
 all_session = []
@@ -45,6 +46,14 @@ def ch_normalizeString(item):
             rtn.append(s)
     return rtn
 
+def replace_lowf(min_frequency,texts):
+    frequency = defaultdict(int)
+    for text in texts:
+        for token in text:
+            frequency[token] += 1
+    texts = [[token if frequency[token] > min_frequency else UNK for token in text] for text in texts]
+    #print(len([i for i in frequency if frequency[i] >= min_frequency]))
+
 for idx,item in df.groupby(df.session_id):
     if(len(idx)>1):
         allpairs[idx] = item
@@ -57,6 +66,6 @@ for x in allpairs.values():
 
 for i,item in enumerate(all_session):
     all_session[i] = ch_normalizeString(item)
-
+replace_lowf(2,all_session)
 np_all_session = np.array(all_session)
 np.save(save_file_path,np_all_session)
